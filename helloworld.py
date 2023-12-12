104173966cf36d908ca8b6b659c33c1db8fd781f
Quinten Krauth <quintenkrauth19@gmail.com>
2023-12-12 16:59:06 -0500

    python re do

---
+++ helloworld.py
@@ -0,0 +1,51 @@
+# print("Hello World")
+
+# first_name = "Quinten"
+# print(first_name)
+
+# age = 25
+# print(age)
+
+# input("Type your name: ")
+
+# print(5 == 4)
+# print (5 != 4)
+
+# Alex_Age = 5
+# Age_Requirement = 5
+
+# if Alex_Age < Age_Requirement:
+#     print("Too Young")
+# elif Alex_Age == Age_Requirement:
+#     print("Aight then")
+# else:
+#     print("Too Old")
+
+# def print_name_twice(text):
+#     print(text)
+#     print(text)
+
+# print_name_twice("Quinten Krauth")
+
+# def age_calculator(age, name):
+#     if age < 5:
+#         print("Baby much?", name, "is", age, "years old")
+#     elif age >= 5 and age <= 10:
+#         print("Fun years...", name, "is", age, "years old")
+#     else:
+#         print("Aight you gettin up there")
+
+# age_calculator(9, "Branson")
+
+# x = 0
+# while (x < 5):
+#     print(x)
+#     x = x + 1
+
+# for y in range(5, 10):
+#     print(y)
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+for d in days:
+    print(d)
